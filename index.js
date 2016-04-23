555ea8efcc81e09c0cecfeac75ed3c50a1e65651
zainul <zainulmasadi90@gmail.com>
2016-04-23 12:08:09 +0700

    adding config name to swagger tags

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ module.exports = function(config) {
   if(!config.namespace) config.namespace = '';
 
   var plural = 's';
+  var oldName = config.name;
 
   if (config.name.substring()[config.name.substring().length - 1] == 'y') {
+    oldName = config.name;
     plural = 'es';
     var replacedName = config.name.slice(0, -1) + 'i';
     config.name = replacedName;
@@ -17,9 +19,9 @@ module.exports = function(config) {
       method: 'GET',
       config: {
         handler: config.controller.index,
-        description: 'Get all '+config.name.replace('_', ' ') + ' records',
-        notes: 'Get all '+config.name.replace('_', ' ') + ' records',
-        tags: ['api']
+        description: 'Get all '+oldName.replace('_', ' ') + ' records',
+        notes: 'Get all '+oldName.replace('_', ' ') + ' records',
+        tags: ['api', oldName]
       }
     },
 
@@ -28,9 +30,9 @@ module.exports = function(config) {
       method: 'GET',
       config: {
         handler: config.controller.show,
-        description: 'Get '+config.name.replace('_', ' ') + ' by id',
-        notes: 'Get all '+config.name.replace('_', ' ') + ' by id return one records',
-        tags: ['api']
+        description: 'Get '+oldName.replace('_', ' ') + ' by id',
+        notes: 'Get all '+oldName.replace('_', ' ') + ' by id return one records',
+        tags: ['api', oldName]
       }
     },
 
@@ -39,10 +41,10 @@ module.exports = function(config) {
       method: 'POST',
       config: {
         handler: config.controller.create,
-        description: 'Create '+config.name.replace('_', ' ') + ' ',
-        notes: 'Create '+config.name.replace('_', ' ') + ' return the model',
+        description: 'Create '+oldName.replace('_', ' ') + ' ',
+        notes: 'Create '+oldName.replace('_', ' ') + ' return the model',
         validate: config.validate || null,
-        tags: ['api']
+        tags: ['api', oldName]
       }
     },
 
@@ -51,10 +53,10 @@ module.exports = function(config) {
       method: 'PUT',
       config: {
         handler: config.controller.update,
-        description: 'Update '+config.name.replace('_', ' ') + ' by id',
-        notes: 'Update '+config.name.replace('_', ' ') + ' by id return the model',
+        description: 'Update '+oldName.replace('_', ' ') + ' by id',
+        notes: 'Update '+oldName.replace('_', ' ') + ' by id return the model',
         validate: config.validate || null,
-        tags: ['api']
+        tags: ['api', oldName]
       }
     },
 
@@ -63,9 +65,9 @@ module.exports = function(config) {
       method: 'DELETE',
       config: {
         handler: config.controller.destroy,
-        description: 'Delete '+config.name.replace('_', ' ') + ' by id',
-        notes: 'Delete '+config.name.replace('_', ' ') + ' by id',
-        tags: ['api']
+        description: 'Delete '+oldName.replace('_', ' ') + ' by id',
+        notes: 'Delete '+oldName.replace('_', ' ') + ' by id',
+        tags: ['api', oldName]
       }
     }
 
